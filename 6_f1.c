4a4a0c38f2229d9faac9335edd4a9366e8343c6a
livia <liviamajzoubsilva19@gmail.com>
2025-04-10 16:59:33 -0300

    Update 6_f1.c
    resposta correta

--- 6_f1.c
+++ 6_f1.c
@@ -16,21 +16,21 @@ int main ()
     media = (n1 + n2 + n3 ) / 3;
 
     if((media > 8) && (frequencia >= 75))
-       printf("O estudante foi aprovado direto pois teve sua media %f e frequencia %d%%", media, frequencia);
+       printf("O estudante foi Aprovado Direto pois teve sua media %f e frequencia %d%%", media, frequencia);
 
     else if((media <=8) && (frequencia >= 75))
-       printf("O estudante foi aprovado com distinçao pois teve sua media %f e frequencia %d%%", media, frequencia);
+       printf("O estudante foi Aprovado com DistinÃ§ao pois teve sua media %f e frequencia %d%%", media, frequencia);
 
     else if((media >= 4) && (media < 6) && (frequencia >= 75)
         ||
         (media >= 6) && (frequencia < 75)
         ||
         (media >= 4) && (frequencia >50) && (frequencia < 75))
-       printf("O estudante vai para final pois teve sua media %f e frequencia %d%%", media, frequencia);
+       printf("O estudante Vai para Final pois teve sua media %f e frequencia %d%%", media, frequencia);
 
     else
 
-    printf("O estudante foi reprovado direto pois teve sua media %f e frequencia %d%%", media, frequencia);
+    printf("O estudante foi Reprovado Direto pois teve sua media %f e frequencia %d%%", media, frequencia);
 
        return 0;
 }
